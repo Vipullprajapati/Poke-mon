e83c70d8104fa03fb00ace65cf1250a8a0b4eeb0
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-10-09 14:40:53 +0000

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
 import data from "./data.json";
-for (let object of data) {
-  console.log(object.name);
-}
+
+const inputEl = document.querySelector("#floatingInputGroup1");
+console.log(inputEl);
+
+// inputEl.focus();
+
+// for (let object of data) {
+//   console.log(object.name);
+// }
+
+document.addEventListener("keyup", (input) => {
+  if (input.key === "/") {
+    // console.log("hey");
+    inputEl.focus();
+  }
+  //   console.log(input.key);
+});
