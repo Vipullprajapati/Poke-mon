542dd528fb08a76ca59afa86b701946dabf26a33
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-10-15 14:26:22 +0000

    filter part done

--- src/js/main.js
+++ src/js/main.js
@@ -8,14 +8,37 @@ const pokemonRow = document.querySelector("[pokemon-row]");
 // console.log(remoteEl);
 // inputEl.focus();
 
-// itreted
-for (let object of data) {
-  const { name, image, description, link } = object;
-  console.log(name, image, description, link);
-  const paragraph = document.createElement("p");
-  paragraph.textContent = name;
-  pokemonRow.appendChild(PokemonCard(name, image, description, link));
+// ===render
+function renderPokemon(list) {
+  pokemonRow.innerHTML = "";
+  for (let object of list) {
+    const { name, image, description, link } = object;
+    const pokemon = PokemonCard(name, image, description, link);
+    pokemonRow.appendChild(pokemon);
+  }
 }
+renderPokemon(data);
+
+// itreted
+// for (let object of data) {
+//   const { name, image, description, link } = object;
+//   console.log(name, image, description, link);
+//   const paragraph = document.createElement("p");
+//   paragraph.textContent = name;
+//   pokemonRow.appendChild(PokemonCard(name, image, description, link));
+// }
+
+// filter functionality
+inputEl.addEventListener("input", (e) => {
+  // console.log(e.target.value);
+  const currInput = e.target.value.toLowerCase().trim();
+  console.log(currInput, data);
+
+  const filterPokemon = data.filter((object) =>
+    object.name.toLocaleLowerCase().includes(currInput)
+  );
+  renderPokemon(filterPokemon);
+});
 
 //  ======add "/" key word =====
 document.addEventListener("keyup", (input) => {
